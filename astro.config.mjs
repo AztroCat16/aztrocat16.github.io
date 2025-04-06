334432df7831d0c85c0869d2f2a027db27f764e3
Angel Sanchez <min3craft0414.angel16@gmail.com>
2025-04-06 04:07:08 -0700

    Correcciones astro.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://aztrocat16.github.io',
-    base: 'unplash',
-    env: {
-        schema: {
-            API_URL: envField.string({default: "hola", optional: false, access: "public", context: "client"})
-        }
-    }
+    base: 'unplash'
 });
