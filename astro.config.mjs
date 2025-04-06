e692d59a845344d28839efdc578bb82933c5b2e7
Angel Sanchez <min3craft0414.angel16@gmail.com>
2025-04-06 03:32:44 -0700

    Se configuran paths para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://aztrocat16.github.io',
+    base: 'unplash',
     env: {
         schema: {
             API_URL: envField.string({default: "hola", optional: false, access: "public", context: "client"})
